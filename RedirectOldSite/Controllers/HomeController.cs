f40002b2ee114c5f38f32ccdc5553079e95f588e
rafaelbittenca <rafaelbittencourt.fns@gmail.com>
2017-10-27 08:54:23 -0400

    Initial

---
+++ RedirectOldSite/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+
+namespace RedirectOldSite.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			return Redirect("http://bittencourt.somee.com");
+		}
+
+		public ActionResult AboutMe()
+		{
+			return Redirect("http://bittencourt.somee.com/Home/AboutMe");
+		}
+	}
+}
